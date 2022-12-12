535fc98a9f908fa666631e27bd5900ebff5d71ce
lukagabadze <gabadzeluka2003@gmail.com>
2022-12-12 21:14:14 +0400

    [HOTFIX] Python Formatting and Diagnostics

--- nvim/lua/format-config/init.lua
+++ nvim/lua/format-config/init.lua
@@ -58,6 +58,8 @@ null_ls.setup({
 		}),
 		null_ls.builtins.code_actions.eslint_d,
 		null_ls.builtins.diagnostics.solhint,
+		null_ls.builtins.formatting.autopep8,
+		null_ls.builtins.diagnostics.flake8,
 	},
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
