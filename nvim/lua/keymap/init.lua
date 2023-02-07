80734f6f8817c452a6240962666bed2fb3b5825b
lukagabadze <gabadzeluka2003@gmail.com>
2023-02-08 03:52:54 +0400

    Updated configs and packages

--- nvim/lua/keymap/init.lua
+++ nvim/lua/keymap/init.lua
@@ -3,6 +3,7 @@ local builtin = require("telescope.builtin")
 -- General --
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "<leader>ec", "<cmd>:edit ~/.config/nvim/init.lua<CR>")
 
 -- Telescope --
 vim.keymap.set("n", "<leader>ll", "<cmd>Explore<CR>")
@@ -10,6 +11,7 @@ vim.keymap.set("n", "<leader>lf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>lb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>lt", "<cmd>:Telescope telescope-tabs list_tabs<CR>", {})
 vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>r", builtin.oldfiles, {})
 vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
 
 -- lsp --
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,67 +1,66 @@
-return require('packer').startup(function()
+return require("packer").startup(function()
 	-- packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 
 	-- colorscheme
-	use 'gruvbox-community/gruvbox'
+	use("gruvbox-community/gruvbox")
 
 	-- web devicons --
-	use 'nvim-tree/nvim-web-devicons'
+	use("nvim-tree/nvim-web-devicons")
 
 	-- lualine --
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
 	-- dashboard --
-	use 'glepnir/dashboard-nvim'
+	use("glepnir/dashboard-nvim")
 
 	-- syntax highlighting--
-	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("windwp/nvim-ts-autotag")
 
 	-- telescope with plugins: tabs
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use 'LukasPietzschmann/telescope-tabs'
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("LukasPietzschmann/telescope-tabs")
 
 	-- auto completion --
-	use 'neovim/nvim-lspconfig'
-	use 'williamboman/nvim-lsp-installer'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
+	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/vim-vsnip")
 
 	-- formatter --
-	use 'jose-elias-alvarez/null-ls.nvim'
-	-- use 'MunifTanjim/prettier.nvim'
-
+	use("jose-elias-alvarez/null-ls.nvim")
 
 	-- which-key --
-	use 'folke/which-key.nvim'
+	use("folke/which-key.nvim")
 
 	-- auto pairs --
-	use {
-		'windwp/nvim-autopairs',
-		config = function() require("nvim-autopairs").setup {} end
-	}
-
-	-- auto tag --
-	use 'alvan/vim-closetag'
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
 	-- git signs --
-	use {
-		'lewis6991/gitsigns.nvim',
+	use({
+		"lewis6991/gitsigns.nvim",
 		config = function()
-			require('gitsigns').setup()
-		end
-	}
+			require("gitsigns").setup()
+		end,
+	})
 
 	-- nvim tree --
-	use 'nvim-tree/nvim-tree.lua'
-
+	use("nvim-tree/nvim-tree.lua")
 end)
