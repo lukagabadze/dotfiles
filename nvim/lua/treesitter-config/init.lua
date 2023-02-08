a7a830fa5ada694975606d01bb4041f03680120b
lukagabadze <77752229+lukagabadze@users.noreply.github.com>
2023-02-08 04:37:58 +0400

    Update 08.02.2023 (#3)
    * Updated configs and packages
    
    * ability to toggle comments
    
    * Configured angular language server

--- nvim/lua/treesitter-config/init.lua
+++ nvim/lua/treesitter-config/init.lua
@@ -1,7 +1,11 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "rust", "javascript", "typescript" },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  }
-}
+require("nvim-ts-autotag").setup()
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "c", "lua", "rust", "javascript", "typescript" },
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	autotag = {
+		enable = true,
+	},
+})
